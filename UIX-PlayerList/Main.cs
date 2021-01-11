8044077156df7b340133ef29b86ce1471f49476c
dan123iel456 <danielj6925@gmail.com>
2021-01-11 14:55:52 -0500

    Add project files.

---
+++ UIX-PlayerList/Main.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UIExpansionKit.API;
+using VRC;
+
+namespace UIX_PlayerList
+{
+    public class Main : MelonMod
+    {
+        LayoutDescription style;
+        bool alphabetical;
+        bool hideSelf;
+
+        private ICustomShowableLayoutedMenu playerMenu;
+
+		public override void OnApplicationStart()
+		{
+            MelonPrefs.RegisterCategory("UIXPlayerList", "UIX Player List");
+            MelonPrefs.RegisterString("UIXPlayerList", "MenuStyle", "List", "Menu Style");
+            MelonPrefs.RegisterBool("UIXPlayerList", "Sort", true, "Alphabetical Sort");
+            MelonPrefs.RegisterBool("UIXPlayerList", "HideSelf", false, "Hide Self");
+        }
+
+
+		public override void VRChat_OnUiManagerInit()
+		{
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Players", delegate ()
+            {
+                loadPlayerMenu();
+                playerMenu.Show();
+            });
+
+            var menuOptions = new[]
+            {
+                ("List", "List"),
+                ("3Grid", "3x3 Grid"),
+                ("4Grid", "4x4 Grid"),
+            };
+            ExpansionKitApi.RegisterSettingAsStringEnum("UIXPlayerList", "MenuStyle", menuOptions);
+
+            updateValues();
+
+        }
+
+		public override void OnModSettingsApplied()
+		{
+            updateValues();
+        }
+
+        public void updateValues()
+		{
+            switch (MelonPrefs.GetString("UIXPlayerList", "MenuStyle"))
+            {
+                case "List":
+                    style = LayoutDescription.WideSlimList;
+                    break;
+                case "3Grid":
+                    style = LayoutDescription.QuickMenu3Columns;
+                    break;
+                case "4Grid":
+                    style = LayoutDescription.QuickMenu4Columns;
+                    break;
+                default:
+                    style = LayoutDescription.WideSlimList;
+                    break;
+            }
+            alphabetical = MelonPrefs.GetBool("UIXPlayerList", "Sort");
+            hideSelf = MelonPrefs.GetBool("UIXPlayerList", "HideSelf");
+        }
+
+
+        public void loadPlayerMenu()
+		{
+            playerMenu = ExpansionKitApi.CreateCustomQuickMenuPage(style);
+
+            playerMenu.AddSimpleButton("<b>~Back~</b>", delegate ()
+            {
+                playerMenu.Hide();
+            });
+
+            if (PlayerManager.field_Private_Static_PlayerManager_0 != null)
+			{
+                Player[] players = PlayerManager.field_Private_Static_PlayerManager_0.field_Private_List_1_Player_0.ToArray();
+                Player[] list = sortPlayerList(players);
+                if(list.Length != 0)
+				{
+                    foreach (Player player in list)
+					{
+                        playerMenu.AddSimpleButton(player.field_Private_APIUser_0.displayName, delegate ()
+                        {
+                            QuickMenu.prop_QuickMenu_0.Method_Public_Void_Player_0(player);
+                            playerMenu.Hide();
+                        });
+
+                    }
+				}
+				else
+				{
+                    playerMenu.AddSimpleButton("List Empty", null);
+                }
+			}
+			else
+			{
+                playerMenu.AddLabel("PlayerList Null");
+			}
+
+        }
+
+        public Player[] sortPlayerList(Player[] players)
+        {
+			if (alphabetical)
+			{
+                for (int i = 1; i < players.Length; i++)
+                {
+                    Player element = players[i];
+                    int j;
+                    for (j = i - 1; j >= 0 && element.field_Private_APIUser_0.displayName.CompareTo(players[j].field_Private_APIUser_0.displayName) <= 0; j--)
+                        players[j + 1] = players[j];
+
+                    players[j + 1] = element;
+                }
+            }
+			if (hideSelf)
+			{
+                List<Player> temp = new List<Player>(); 
+                foreach (Player tempPlayer in players)
+				{
+                    if(tempPlayer.field_Private_APIUser_0.id != VRCPlayer.field_Internal_Static_VRCPlayer_0.field_Private_Player_0.field_Private_APIUser_0.id)
+					{
+                        temp.Add(tempPlayer);
+					}
+				}
+                players = temp.ToArray<Player>();
+			}
+            
+            return players;
+        }
+
+    }
+}
---
+++ UIX-PlayerList/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UIX-PlayerList")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UIX-PlayerList")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e1d4cc6-ad28-4a27-9b8f-871c8e2c3384")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(UIX_PlayerList.Main), "UIX Player List", "1.0.0", "Dannie")]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonOptionalDependencies("UIExpansionKit")]
